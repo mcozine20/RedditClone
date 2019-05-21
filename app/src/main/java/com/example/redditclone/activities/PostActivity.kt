81f74772e34f5a731e9071d301254291091de2f1
mclean <mcozine20@amherst.edu>
2019-05-21 17:35:30 +0200

    started on PostActivity

--- app/src/main/java/com/example/redditclone/activities/PostActivity.kt
+++ app/src/main/java/com/example/redditclone/activities/PostActivity.kt
@@ -3,7 +3,19 @@ package com.example.redditclone.activities
 import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import com.example.redditclone.R
+import kotlinx.android.synthetic.main.activity_post.*
 
 class PostActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_post)
+
+        var intentThatStartedThis = getIntent()
+        var postTitle = intentThatStartedThis.getStringExtra(Intent.EXTRA_TEXT)
+
+        tvPostTitle.text = postTitle
+    }
 
 }
