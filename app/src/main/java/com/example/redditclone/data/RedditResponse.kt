c3beffd2b943dca329dfac37af760e2bad1320c8
crabasa0 <c.rabasa0@gmail.com>
2019-05-21 15:56:23 +0200

    Added first version of the RedditResponse data class

---
+++ app/src/main/java/com/example/redditclone/data/RedditResponse.kt
@@ -0,0 +1,9 @@
+package com.example.redditclone.data
+
+data class RedditResponse(val kind:String?, val data:Data?)
+
+data class Data(val modhash:String?, val dist:Int?, val children:List<RedditPost>?, val after:String?, val before:String?)
+
+data class RedditPost(val kind:String?, val data:PostData?)
+
+data class PostData(val subreddit:String?, val selftext: String?, val title:String?, val thumbnail:String?, val author:String?, val url:String?)
